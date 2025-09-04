726cebf5690161630df98666713ceb6c3f14bd88
KyeongMinBae <reaaaaaaal@gmail.com>
2025-09-05 00:48:13 +0900

    2차원 배열 - 리팩토링2

--- src/array/ArrDi3.java
+++ src/array/ArrDi3.java
@@ -1,18 +1,13 @@
 package array;
 
-public class ArrDi2 {
+public class ArrDi3 {
 
     public static void main(String[] args) {
         // 2x3 2차원 배열을 만든다.
-        int[][] arr = new int[2][3]; //행2, 열3
-
-        arr[0][0] = 1; //0행, 0열
-        arr[0][1] = 2; //0행, 1열
-        arr[0][2] = 3; //0행, 2열
-        arr[1][0] = 4; //1행, 0열
-        arr[1][1] = 5; //1행, 1열
-        arr[1][2] = 6; //1행, 2열
-
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        }; //행2, 열3
 
         //0행 출력
         for (int row = 0; row < arr.length; row++) {
--- src/array/ArrDi4.java
+++ src/array/ArrDi4.java
@@ -1,18 +1,17 @@
 package array;
 
-public class ArrDi2 {
+public class ArrDi4 {
 
     public static void main(String[] args) {
         // 2x3 2차원 배열을 만든다.
-        int[][] arr = new int[2][3]; //행2, 열3
+        int[][] arr = new int[10][10];
 
-        arr[0][0] = 1; //0행, 0열
-        arr[0][1] = 2; //0행, 1열
-        arr[0][2] = 3; //0행, 2열
-        arr[1][0] = 4; //1행, 0열
-        arr[1][1] = 5; //1행, 1열
-        arr[1][2] = 6; //1행, 2열
-
+        int i = 1;
+        for (int row = 0; row < arr.length; row++){
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = i++;
+            }
+        }
 
         //0행 출력
         for (int row = 0; row < arr.length; row++) {
